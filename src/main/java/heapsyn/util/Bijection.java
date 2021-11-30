774b40655d13726073ff7ede6e35dd306f9eed2b
zyf0726 <zyf0726@pku.edu.cn>
2021-11-30 19:22:13 +0800

    refactor

--- src/main/java/heapsyn/util/Bijection.java
+++ src/main/java/heapsyn/util/Bijection.java
@@ -1,71 +1,67 @@
 package heapsyn.util;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import com.google.common.base.Preconditions;
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
 import com.google.common.collect.ImmutableMap;
 
-// FIXME replace this class by BiMap
+// TODO replace this class by BiMap
 
 public class Bijection<U, V> {
 	
-	private Map<U, V> mapU2V;
-	private Map<V, U> mapV2U;
+	private BiMap<U, V> bimap;
 	
 	public Bijection() {
-		this.mapU2V = new HashMap<>();
-		this.mapV2U = new HashMap<>();
+		this.bimap = HashBiMap.create();
 	}
 	
 	public Bijection(Bijection<U, V> other) {
-		this.mapU2V = new HashMap<>(other.mapU2V);
-		this.mapV2U = new HashMap<>(other.mapV2U);
+		this.bimap = HashBiMap.create(other.bimap);
 	}
 	
 	public boolean putUV(U keyU, V keyV) {
 		Preconditions.checkNotNull(keyU);
 		Preconditions.checkNotNull(keyV);
-		if (mapU2V.containsKey(keyU))
-			return keyV.equals(mapU2V.get(keyU));
-		if (mapV2U.containsKey(keyV))
+		if (this.bimap.containsKey(keyU))
+			return keyV.equals(this.bimap.get(keyU));
+		if (this.bimap.containsValue(keyV))
 			return false;
-		mapU2V.put(keyU, keyV);
-		mapV2U.put(keyV, keyU);
+		this.bimap.put(keyU, keyV);
 		return true;
 	}
 	
 	public V getV(U keyU) {
-		return this.mapU2V.get(keyU);
+		return this.bimap.get(keyU);
 	}
 	
 	public U getU(V keyV) {
-		return this.mapV2U.get(keyV);
+		return this.bimap.inverse().get(keyV);
 	}
 	
 	public boolean containsU(U keyU) {
-		return this.mapU2V.containsKey(keyU);
+		return this.bimap.containsKey(keyU);
 	}
 	
 	public boolean containsV(V keyV) {
-		return this.mapV2U.containsKey(keyV);
+		return this.bimap.containsValue(keyV);
 	}
 	
 	public Map<U, V> getMapU2V() {
-		return ImmutableMap.copyOf(this.mapU2V);
+		return ImmutableMap.copyOf(this.bimap);
 	}
 	
 	public Map<V, U> getMapV2U() {
-		return ImmutableMap.copyOf(this.mapV2U);
+		return ImmutableMap.copyOf(this.bimap.inverse());
 	}
 	
 	public int size() {
-		return this.mapU2V.size();
+		return this.bimap.size();
 	}
 	
 	public void clear() {
-		this.mapU2V.clear();
-		this.mapV2U.clear();
+		this.bimap.clear();
 	}
 
 }
